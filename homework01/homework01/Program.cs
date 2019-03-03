4ee564e3ae05936ee10e8aa9fcc0d9bdeabe0b57
3017218198 <lljlljly@tju.edu.cn>
2019-03-03 18:13:27 +0800

    first edition

---
+++ homework01/homework01/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace homework01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Green;
+            System.Console.WriteLine("Hello World!");
+            System.Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.Yellow;
+            System.Console.WriteLine("Hello World!");
+            /* 
+             * 1.reset the background color to defalut
+             * 2.reset the foreground color to default
+             */
+            Console.BackgroundColor = ConsoleColor.Black;
+            System.Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.White;
+            System.Console.WriteLine("Hello World!");
+        }
+    }
+}
